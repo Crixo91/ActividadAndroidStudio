5b34a56562648a3cf02df13146adb794568cf47b
cristopherMolina <cristopher.moli@gmail.com>
2022-09-20 20:00:40 -0300

    v.1.0

---
+++ app/src/main/java/com/example/actividadandroid/Cuestionario.java
@@ -0,0 +1,16 @@
+package com.example.actividadandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cuestionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuestionario);
+    }
+
+
+}
